a08f3c56ff44a17b08a706caeb18760f02e95f57
mxl <1759591150@qq.com>
2026-07-05 20:52:26 +0800

    github worktion

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,22 +1,33 @@
 pluginManagement {
     repositories {
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
-        maven("https://maven.aliyun.com/repository/public")
-        google()
-        mavenCentral()
-        gradlePluginPortal()
+        if (providers.environmentVariable("CI").isPresent) {
+            google()
+            mavenCentral()
+            gradlePluginPortal()
+        } else {
+            maven("https://maven.aliyun.com/repository/google")
+            maven("https://maven.aliyun.com/repository/gradle-plugin")
+            maven("https://maven.aliyun.com/repository/public")
+            google()
+            mavenCentral()
+            gradlePluginPortal()
+        }
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/public")
-        maven("https://maven.aliyun.com/repository/central")
-        google()
-        mavenCentral()
+        if (providers.environmentVariable("CI").isPresent) {
+            google()
+            mavenCentral()
+        } else {
+            maven("https://maven.aliyun.com/repository/google")
+            maven("https://maven.aliyun.com/repository/public")
+            maven("https://maven.aliyun.com/repository/central")
+            google()
+            mavenCentral()
+        }
     }
 }
 
